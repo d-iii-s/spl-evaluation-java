34892307a4aaf822ac548e2b1387ea3af2817c89
Vojtech Horky <vojtech.horky@gmail.com>
2015-05-27 13:36:34 +0200

    Learning interpretation: rewrite

--- src/java/cz/cuni/mff/d3s/spl/interpretation/DistributionLearningInterpretationParallel.java
+++ src/java/cz/cuni/mff/d3s/spl/interpretation/DistributionLearningInterpretationParallel.java
@@ -17,6 +17,7 @@
 package cz.cuni.mff.d3s.spl.interpretation;
 
 import java.util.ArrayList;
+import java.util.Collection;
 import java.util.Random;
 import java.util.concurrent.Callable;
 import java.util.concurrent.ExecutionException;
@@ -86,8 +87,10 @@ public class DistributionLearningInterpretationParallel implements Interpretatio
 		try {
 			return compareThrowing(left, right);
 		} catch (InterruptedException e) {
+			e.printStackTrace();
 			return null;
 		} catch (ExecutionException e) {
+			e.printStackTrace();
 			return null;
 		}
 	}
@@ -101,11 +104,12 @@ public class DistributionLearningInterpretationParallel implements Interpretatio
 		@SuppressWarnings("unchecked")
 		Future<double[]>[] samplesBeforeDiff = (Future<double[]>[]) new Future<?>[2];
 		
+		Future<Double> learningMean = executor.submit(new MeanComputation(learningSets[0], learningSets[1]));
 		for (int i = 0; i < 2; i++) {
-			Future<Double> mean = executor.submit(new MeanComputation(learningSets[i]));
-			Future<double[]> samples = executor.submit(new DoubleBootstrap(learningSets[i], bootstrapSizeInnerMeans, bootstrapSizeOuterMeans, executor));
-			Future<double[]> samplesNormalized = executor.submit(new SubtractFromArray(samples, mean));
-			samplesBeforeDiff[i] = executor.submit(new Bootstrap(samplesNormalized, diffDistributionSampleCount));
+			Future<double[][]> allSamples = executor.submit(new RunsToDoubleArrays(learningSets[i]));
+			Future<double[][]> samplesShifted = executor.submit(new SubtractFrom2DArray(allSamples, learningMean));
+			Future<double[]> boostrapped = executor.submit(new DoubleBootstrap(samplesShifted, bootstrapSizeInnerMeans, bootstrapSizeOuterMeans, executor));
+			samplesBeforeDiff[i] = executor.submit(new Bootstrap(boostrapped, diffDistributionSampleCount));
 		}
 		
 		Future<double[]> diffSamplesFuture = executor.submit(new ArrayDiff(samplesBeforeDiff[0], samplesBeforeDiff[1]));
@@ -165,32 +169,59 @@ public class DistributionLearningInterpretationParallel implements Interpretatio
 		}
 	}
 	
+	
 	private static class MeanComputation implements Callable<Double> {
-		private final DataSnapshot data;
+		private final DataSnapshot[] data;
 		
-		public MeanComputation(DataSnapshot d) {
+		public MeanComputation(DataSnapshot... d) {
 			data = d;
 		}
 
 		@Override
 		public Double call() throws Exception {
-			BenchmarkRun merged = BenchmarkRunUtils.merge(data.getRuns());
-			
-			BenchmarkRunSummary summary = new BenchmarkRunSummary(merged);
+			Collection<BenchmarkRun> merged = new ArrayList<>(data.length);
+			for (DataSnapshot d : data) {
+				merged.add(BenchmarkRunUtils.merge(d.getRuns()));
+			}
+			BenchmarkRun twiceMerged = BenchmarkRunUtils.merge(merged);
+				
+			BenchmarkRunSummary summary = new BenchmarkRunSummary(twiceMerged);
 			
 			return summary.getMean();
 		}
 	}
 	
+	private static class RunsToDoubleArrays implements Callable<double[][]> {
+		private final DataSnapshot data;
+		
+		public RunsToDoubleArrays(DataSnapshot d) {
+			data = d;			
+		}
+
+		@Override
+		public double[][] call() throws Exception {
+			double[][] result = new double[data.getRunCount()][];
+			
+			int index = 0;
+			for (BenchmarkRun run : data.getRuns()) {
+				result[index] = BenchmarkRunUtils.toDoubleArray(run);
+				index++;
+			}
+			
+			return result;
+		}
+		
+	}
+	
 	private static class DoubleBootstrap implements Callable<double[]> {
 		private final ExecutorService executor;
-		private final DataSnapshot data;
+		private final Future<double[][]> dataFuture;
 		private final int bootstrapSizeInnerMeans;
 		private final int bootstrapSizeOuterMeans;
-		private final Random bootstrapRandom = new Random(0);
+		private final Random bootstrapRandom = new Random();
 		
-		public DoubleBootstrap(DataSnapshot d, int innerSize, int outerSize, ExecutorService exec) {
-			data = d;
+		public DoubleBootstrap(Future<double[][]> dataFuture, int innerSize, int outerSize, ExecutorService exec) {
+			this.dataFuture = dataFuture;
 			bootstrapSizeInnerMeans = innerSize;
 			bootstrapSizeOuterMeans = outerSize;
 			executor = exec;
@@ -198,12 +229,13 @@ public class DistributionLearningInterpretationParallel implements Interpretatio
 		
 		@Override
 		public double[] call() throws Exception {
-			int runCount = data.getRunCount();
+			double[][] data = dataFuture.get();
+			int runCount = data.length;
 			
 			double[] runMeans = new double[runCount * bootstrapSizeInnerMeans];
 			ArrayList<Callable<Void>> tasks = new ArrayList<>(runCount);
 			for (int i = 0; i < runCount; i++) {
-				tasks.add(new BootstrapFromRun(data.getRun(i), runMeans, i * bootstrapSizeInnerMeans, bootstrapSizeInnerMeans));
+				tasks.add(new MeanBootstrap(data[i], runMeans, i * bootstrapSizeInnerMeans, bootstrapSizeInnerMeans));
 			}
 			
 			executor.invokeAll(tasks);
@@ -215,13 +247,13 @@ public class DistributionLearningInterpretationParallel implements Interpretatio
 		}
 	}
 	
-	private static class BootstrapFromRun implements Callable<Void> {
+	private static class MeanBootstrap implements Callable<Void> {
 		private final double[] fullArray;
 		private final int myStartIndex;
 		private final int myLength;
-		private final BenchmarkRun myRun;
+		private final double[] myRun;
 		
-		public BootstrapFromRun(BenchmarkRun run, double[] array, int startIndex, int length) {
+		public MeanBootstrap(double[] run, double[] array, int startIndex, int length) {
 			myRun = run;
 			fullArray = array;
 			myStartIndex = startIndex;
@@ -230,31 +262,32 @@ public class DistributionLearningInterpretationParallel implements Interpretatio
 		
 		@Override
 		public Void call() throws Exception {
-			double[] samples = BenchmarkRunUtils.toDoubleArray(myRun);
-			bootstrapWithMean(new Random(0), samples, samples.length, myLength, fullArray, myStartIndex);
+			bootstrapWithMean(new Random(0), myRun, myRun.length, myLength, fullArray, myStartIndex);
 			return null;
 		}
 	}
 	
-	private static class SubtractFromArray implements Callable<double[]> {
-		private final Future<double[]> arrayFuture;
+	private static class SubtractFrom2DArray implements Callable<double[][]> {
+		private final Future<double[][]> arrayFuture;
 		private final Future<Double> constantFuture;
 		
-		public SubtractFromArray(Future<double[]> arr, Future<Double> c) {
+		public SubtractFrom2DArray(Future<double[][]> arr, Future<Double> c) {
 			arrayFuture = arr;
 			constantFuture = c;
 		}
 
 		@Override
-		public double[] call() throws Exception {
-			double[] array = arrayFuture.get();
+		public double[][] call() throws Exception {
+			double[][] arrays = arrayFuture.get();
 			double c = constantFuture.get();
 			
-			for (int i = 0; i < array.length; i++) {
-				array[i] -= c;
+			for (int i = 0; i < arrays.length; i++) {
+				for (int j = 0; j < arrays[i].length; j++) {
+					arrays[i][j] -= c;
+				}
 			}
 			
-			return array;
+			return arrays;
 		}
 	}
 	
